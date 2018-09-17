7dca9971977ed33edee05da606df7b4dfe98ada3
Luke Nelson <lukenels@cs.washington.edu>
2018-09-17 20:37:45 +0000

    fix cat

--- webrcon.py
+++ webrcon.py
@@ -1,11 +1,11 @@
 
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return redirect('/static/hello.html')
+    return app.send_static_file('hello.html')
 
 def create_app():
     return app
