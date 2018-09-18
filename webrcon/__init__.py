4d0c69d9e5f2fdf67d153817cb46436756d1de65
Luke Nelson <lukenels@cs.washington.edu>
2018-09-17 17:21:31 -0700

    update

--- webrcon/__init__.py
+++ webrcon/__init__.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 import os
 
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('config.py')
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -20,6 +20,6 @@ def create_app(test_config=None):
 
     @app.route('/')
     def hello():
-        return app.send_static_file('hello.html')
+        return render_template('hello.html')
 
     return app
