f87369812e728caf295fafa061044fe36ab4dbbe
Luke Nelson <lukenels@cs.washington.edu>
2018-09-21 13:32:54 -0700

    .

--- webrcon/internal.py
+++ webrcon/internal.py
@@ -1,10 +1,21 @@
 
 from flask import Blueprint, g, render_template, request
 
+from .db import get_db
+
 bp = Blueprint('internal', __name__, url_prefix='/internal')
 
 @bp.route('/diamonds', methods=('POST',))
 def diamonds():
     print(request.form['number'])
 
-    return None
+    db = get_db()
+
+    db.execute(
+        "INSERT INTO diamonds (number, timestamp) VALUES (?, datetime('%s', 'now', 'unixepoch'))",
+        (request.form.get('number'),)
+    )
+
+    db.commit()
+
+    return ""
--- webrcon/schema.sql
+++ webrcon/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS diamonds;
 
 CREATE TABLE diamonds (
-  number INTEGER NOT NULL
+  number INTEGER NOT NULL,
+  timestamp TEXT NOT NULL
 );
